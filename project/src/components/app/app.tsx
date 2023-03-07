8a22530661f9c22ce63c0d1a3a438b8b79268d95
Ksenia <ksenechka.93@gmail.com>
2023-03-07 09:47:53 +0300

    add router, bug fixes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
 import MainScreen from '../../pages/main-screen/main-screen';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MyListScreen from '../../pages/my-list-screen/my-list-screen';
+import AuthorizationScreen from '../../pages/authorization-screen/authorization-screen';
+import MoviePageScreen from '../../pages/movie-page-screen/movie-page-screen';
+import AddReviewScreen from '../../pages/add-review-screen/add-review-screen';
+import PlayerScreen from '../../pages/player-screen/player-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found';
+import { AppRoute } from '../../const';
+
 
 type MainScreenProp = {
   title: string;
@@ -8,12 +17,42 @@ type MainScreenProp = {
 
 function App({title, genre, year}: MainScreenProp): JSX.Element {
   return (
-    <MainScreen
-      title = {title}
-      genre = {genre}
-      year = {year}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element = {<MainScreen title={title} genre={genre} year={year}/>}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<AuthorizationScreen />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePageScreen />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          //TODO
+          element={<MyListScreen/>}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          //TODO
+          element={<AddReviewScreen/>}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ project/src/pages/not-found-screen/not-found.tsx
@@ -0,0 +1,12 @@
+function NotFoundScreen(): JSX.Element {
+  return (
+  //TODO
+    <div>
+      <p>
+      Not found!
+      </p>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
