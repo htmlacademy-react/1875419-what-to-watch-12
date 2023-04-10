b10571f3de24f2031598784ece3e85f98237e990
Ksenia <ksenechka.93@gmail.com>
2023-04-10 23:34:03 +0300

    fix:  reviews divided in two columns

--- project/src/components/film-tabs/film-tab-reviews.tsx
+++ project/src/components/film-tabs/film-tab-reviews.tsx
@@ -11,9 +11,8 @@ function FilmTabReviews({reviews}: ReviewsProp): JSX.Element {
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
-        {/* TODO: сделать вывод по три комментария в колонке */}
-        {filmReviews.map((review) => {
-          const keyValue = `${review.id}-${review.user.id}-${review.date}`;
+        {filmReviews.slice(0, Math.ceil(reviews.length / 2)).map((review) => {
+          const keyValue = `${review.id}-${review.user.id}`;
           const date = new Date(review.date);
           return (
             <div className="review" key={keyValue}>
@@ -31,7 +30,25 @@ function FilmTabReviews({reviews}: ReviewsProp): JSX.Element {
           );
         })}
       </div>
+      <div className="film-card__reviews-col">
+        {reviews.slice(Math.ceil(reviews.length / 2)).map((review) => {
+          const keyValue = `${review.id}-${review.user.id}`;
+          const date = new Date(review.date);
+          return (
+            <div className="review" key={keyValue}>
+              <blockquote className="review__quote">
+                <p className="review__text">{review.comment}</p>
 
+                <footer className="review__details">
+                  <cite className="review__author">{review.user.name}</cite>
+                  <time className="review__date" dateTime={review.date}>{date.toLocaleDateString(LOCALE, {month: 'long', day: 'numeric', year: 'numeric'})}</time>
+                </footer>
+              </blockquote>
+
+              <div className="review__rating">{review.rating}</div>
+            </div>
+          ); })}
+      </div>
     </div>
   );
 }
